452a3fc1801173156bdaecf8a97cfd279c0e8944
DZataidukh <dz@letyshops.com>
2019-12-26 12:33:48 +0200

    2.0.0-beta.2

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export { Model } from './mc/Model';
 export { Collection } from './mc/Collection';
 export { ssrMCPlugin } from './mc/plugin';
-export * from './types/TApiConf'
-export * from './types/plugin'
-export * from './types/IModel'
-export * from './types/ICollection'
+export * from './types/TApiConf';
+export * from './types/plugin';
+export * from './types/IModel';
+export * from './types/ICollection';
